4f322c5130e6e4a7438eab491ae8e8a5e8291eed
Rey Madrid <enokslaboratory@fullsail.edu>
2013-10-19 13:37:59 -0700

    rake db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,27 @@
 
 ActiveRecord::Schema.define(version: 20131019032936) do
 
+  create_table "0subject", primary_key: "subjectId", force: true do |t|
+    t.string "subjectname", limit: 11, null: false
+  end
+
+  create_table "0users", id: false, force: true do |t|
+    t.string "user_name",     limit: 20, default: "", null: false
+    t.string "user_password", limit: 32, default: "", null: false
+    t.string "first_name",    limit: 20,              null: false
+    t.string "last_name",     limit: 20,              null: false
+    t.string "user_salt",     limit: 8,  default: "", null: false
+  end
+
+  add_index "0users", ["user_name", "user_password"], name: "UX_name_password", unique: true, using: :btree
+  add_index "0users", ["user_name"], name: "UX_name", unique: true, using: :btree
+
+  create_table "0video", primary_key: "videoId", force: true do |t|
+    t.string  "title",     limit: 40,              null: false
+    t.string  "icon",      limit: 20, default: "", null: false
+    t.integer "subjectId"
+  end
+
   create_table "admin_users", force: true do |t|
     t.string   "first_name",      limit: 25
     t.string   "last_name",       limit: 50
@@ -26,33 +47,12 @@ ActiveRecord::Schema.define(version: 20131019032936) do
 
   add_index "admin_users", ["username"], name: "index_admin_users_on_username", using: :btree
 
-  create_table "subject1", primary_key: "subjectId", force: true do |t|
-    t.string "subjectname", limit: 11, null: false
-  end
-
   create_table "subjects", force: true do |t|
     t.string   "name",       limit: 25
     t.integer  "position"
     t.boolean  "visible",               default: false
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "users1", id: false, force: true do |t|
-    t.string "user_name",     limit: 20, default: "", null: false
-    t.string "user_password", limit: 32, default: "", null: false
-    t.string "first_name",    limit: 20,              null: false
-    t.string "last_name",     limit: 20,              null: false
-    t.string "user_salt",     limit: 8,  default: "", null: false
-  end
-
-  add_index "users1", ["user_name", "user_password"], name: "UX_name_password", unique: true, using: :btree
-  add_index "users1", ["user_name"], name: "UX_name", unique: true, using: :btree
-
-  create_table "video1", primary_key: "videoId", force: true do |t|
-    t.string  "title",     limit: 40,              null: false
-    t.string  "icon",      limit: 20, default: "", null: false
-    t.integer "subjectId"
   end
 
   create_table "videos", force: true do |t|
